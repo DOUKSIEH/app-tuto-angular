cd27c83a66bce7b7b8423e3a19df911fa8c7b426
DOUKSIEH <douksieha@gmail.com>
2019-10-06 09:04:30 +0200

    modification styleUrls

--- src/app/hotel/hotel.component.ts
+++ src/app/hotel/hotel.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hotel',
   templateUrl: './hotel.component.html',
-  styleUrls:  ['./hotel.component.css']
+  styleUrls:  ['./hotel.component.scss']
 
   })
 export class HotelComponent implements OnInit {
